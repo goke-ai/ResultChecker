131a1ba7662169b551966c844a2f880c423042bd
Olagoke Oladokun <gokeladokun@hotmail.com>
2021-02-09 18:24:32 +0100

    Add Grades

---
+++ src/ResultCheckerBwaApp/Client/wwwroot/storage.js
@@ -0,0 +1,23 @@
+﻿window.localStorageSetItem = (key, value) => {
+    localStorage.setItem(key, value);
+};
+
+window.localStorageGetItem = (key) => {
+    return localStorage.getItem(key);
+};
+
+window.localStorageClear = () => {
+    localStorage.clear();
+};
+
+window.sessionStorageSetItem = (key, value) => {
+    sessionStorage.setItem(key, value);
+};
+
+window.sessionStorageGetItem = (key) => {
+    return sessionStorage.getItem(key);
+};
+
+window.sessionStorageClear = () => {
+    sessionStorage.clear();
+};
